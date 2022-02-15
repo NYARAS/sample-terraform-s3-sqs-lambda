c1ffc9d2eb16b25d43ef6f694352b5aa8262107e
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-15 15:39:47 +0300

    chore: style and format the code
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # Data resource to archive Lambda function code
 data "archive_file" "lambda_zip" {
-    source_dir  = "${path.module}/lambda/"
-    output_path = "${path.module}/lambda.zip"
-    type        = "zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/lambda.zip"
+  type        = "zip"
 }
 
 # Lambda function policy
 resource "aws_iam_policy" "lambda_policy" {
-    name        = "${var.app_env}-lambda-policy"
-    description = "${var.app_env}-lambda-policy"
- 
-    policy = <<EOF
+  name        = "${var.app_env}-lambda-policy"
+  description = "${var.app_env}-lambda-policy"
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -47,8 +47,8 @@ EOF
 
 # Lambda function role
 resource "aws_iam_role" "iam_for_terraform_lambda" {
-    name = "${var.app_env}-lambda-role"
-    assume_role_policy = <<EOF
+  name               = "${var.app_env}-lambda-role"
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -66,23 +66,23 @@ EOF
 
 # Role to Policy attachment
 resource "aws_iam_role_policy_attachment" "terraform_lambda_iam_policy_basic_execution" {
-    role = aws_iam_role.iam_for_terraform_lambda.id
-    policy_arn = aws_iam_policy.lambda_policy.arn
+  role       = aws_iam_role.iam_for_terraform_lambda.id
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
 
 # Lambda function declaration
 resource "aws_lambda_function" "sqs_processor" {
-    filename = "lambda.zip"
-    source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-    function_name = "${var.app_env}-lambda"
-    role = aws_iam_role.iam_for_terraform_lambda.arn
-    handler = "index.handler"
-    runtime = "python3.8"
+  filename         = "lambda.zip"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  function_name    = "${var.app_env}-lambda"
+  role             = aws_iam_role.iam_for_terraform_lambda.arn
+  handler          = "index.handler"
+  runtime          = "python3.8"
 }
 
 # CloudWatch Log Group for the Lambda function
 resource "aws_cloudwatch_log_group" "lambda_loggroup" {
-    name = "/aws/lambda/${aws_lambda_function.sqs_processor.function_name}"
-    retention_in_days = 14
+  name              = "/aws/lambda/${aws_lambda_function.sqs_processor.function_name}"
+  retention_in_days = 14
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    default = "eu-west-1"
-    description = "AWS Region to deploy to"
+  default     = "eu-west-1"
+  description = "AWS Region to deploy to"
 }
 
 variable "app_env" {
-    default = "calvine"
-    description = "Common prefix for all Terraform created resources"
+  default     = "calvine"
+  description = "Common prefix for all Terraform created resources"
 }
