c128db3393703523bf74351243689f6c64158b40
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-15 15:28:39 +0300

    chore: add sqs queue
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ s3.tf
@@ -0,0 +1,21 @@
+# SQS queue
+resource "aws_sqs_queue" "queue" {
+  name = "${var.app_env}-s3-event-notification-queue"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:${var.app_env}-s3-event-notification-queue",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
