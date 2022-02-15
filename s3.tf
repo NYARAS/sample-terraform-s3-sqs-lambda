1b1f10531d15585ea939e2d5afafb3ae005d3857
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-15 15:30:48 +0300

    chore: add lambda sqs event source mapping
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- s3.tf
+++ s3.tf
@@ -34,3 +34,11 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     events        = ["s3:ObjectCreated:*"]
   }
 }
+
+# Event source from SQS
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  event_source_arn = aws_sqs_queue.queue.arn
+  enabled          = true
+  function_name    = aws_lambda_function.sqs_processor.arn
+  batch_size       = 1
+}
