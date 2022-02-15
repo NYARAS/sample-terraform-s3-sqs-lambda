1e0281cd518314696609e1e83f1a76ed784c8b68
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-15 15:36:34 +0300

    chore: add lambda function policy, role and role attachment
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- main.tf
+++ main.tf
@@ -4,3 +4,68 @@ data "archive_file" "lambda_zip" {
     output_path = "${path.module}/lambda.zip"
     type        = "zip"
 }
+
+# Lambda function policy
+resource "aws_iam_policy" "lambda_policy" {
+    name        = "${var.app_env}-lambda-policy"
+    description = "${var.app_env}-lambda-policy"
+ 
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    },
+    {
+      "Action": [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_sqs_queue.queue.arn}"
+    },
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Lambda function role
+resource "aws_iam_role" "iam_for_terraform_lambda" {
+    name = "${var.app_env}-lambda-role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Role to Policy attachment
+resource "aws_iam_role_policy_attachment" "terraform_lambda_iam_policy_basic_execution" {
+    role = aws_iam_role.iam_for_terraform_lambda.id
+    policy_arn = aws_iam_policy.lambda_policy.arn
+}
