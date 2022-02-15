c040c0d742c1825b8056d6b78d5e9792ceb17adb
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-15 15:29:51 +0300

    chore: add s3 bucket and s3 bucket notification
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- s3.tf
+++ s3.tf
@@ -19,3 +19,18 @@ resource "aws_sqs_queue" "queue" {
 }
 POLICY
 }
+
+# S3 bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.app_env}-s3-sqs-demo-bucket"
+}
+
+# S3 event filter
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
